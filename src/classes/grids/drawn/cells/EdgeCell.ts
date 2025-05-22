8c60d6db677a5acb30d66ca02cc9acf5130a9f85
koljapluemer <github@koljapluemer.com>
2025-05-22 13:49:11 +0200

    start config for edge ascii render

--- src/classes/grids/drawn/cells/EdgeCell.ts
+++ src/classes/grids/drawn/cells/EdgeCell.ts
@@ -1,6 +1,8 @@
 import DrawnEdge from "../DrawnEdge";
 import Cell, { CellType } from "./Cell";
 
+
+
 export default class EdgeCell extends Cell {
 
 	edge: DrawnEdge
@@ -19,7 +21,51 @@ export default class EdgeCell extends Cell {
 		return "edgeCell"
 	}
 
+
+	public getHumanReadableDataString(): string {
+		const dataString = "----_----"
+		const chars = dataString.split('')
+
+		if (this.connectsToNorth) {
+			chars[0] = "N"
+		}
+		if (this.connectsToEast) {
+			chars[1] = "E"
+		}
+		if (this.connectsToSouth) {
+			chars[2] = "S"
+		}
+		if (this.connectsToWest) {
+			chars[3] = "W"
+		}
+
+		if (this.hasArrowNorth) {
+			chars[5] = "n"
+		}
+		if (this.hasArrowEast) {
+			chars[6] = "e"
+		}
+		if (this.hasArrowSouth) {
+			chars[7] = "s"
+		}
+		if (this.hasArrowWest) {
+			chars[8] = "w"
+		}
+
+		return chars.join('')
+	}
+
 	public renderAsASCII(): string {
-		return "x"
+		const pattern = this.getHumanReadableDataString()
+		switch (pattern) {
+			case "-E-W_----": return "—"
+			case "-E-W_-e--": return "→"
+			case "-E-W_---w": return "←"
+			case "N-S-_----": return "|"
+			case "N-S-_n---": return "↑"
+			case "N-S-_--s-": return "↓"
+			case "NESW_----": return "🞡"
+			default: return "~"
+		}
 	}	
 }
